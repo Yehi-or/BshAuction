3d1340db39222dd6ffcf7cf64410f33247bbc05d
BSH <qustjdghks1545@gmail.com>
2023-07-25 00:12:49 +0900

    Add Redis searchRanking

--- SpringBack/src/main/java/com/bsh/bshauction/BshAuctionApplication.java
+++ SpringBack/src/main/java/com/bsh/bshauction/BshAuctionApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableAsync
+@EnableScheduling
 public class BshAuctionApplication {
 
     public static void main(String[] args) {
--- SpringBack/src/main/java/com/bsh/bshauction/service/RedisService.java
+++ SpringBack/src/main/java/com/bsh/bshauction/service/RedisService.java
@@ -4,10 +4,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
-import java.time.Instant;
 import java.util.*;
 
 @Service
@@ -16,58 +15,75 @@ import java.util.*;
 public class RedisService {
     private final RedisTemplate<String, String> redisTemplate;
     private static final String SEARCH_WEIGHT_PREFIX = "search_weight:";
+    private double totalSearchFrequency = 0;
 
+    @Async
     public void doSearch(String keyword) {
+
+        double weight = findSearchKeywordWeight(keyword);
+
+        log.info("weight : {}", weight);
+        redisTemplate.opsForZSet().incrementScore("search_ranking", keyword, weight);
+
+        //1시간 동안 특정 검색어가 얼마나 증가한지 확인
+        redisTemplate.opsForZSet().incrementScore("search_keyword_forOneHour", keyword, 1);
+        totalSearchFrequency++;
+    }
+
+    // 1시간 마다 평균 검색 횟수를 구하는 메서드 가중치를 주고 싶은데 후에 추가할 예정 2023-07-24
+    @Scheduled(fixedRate = 60 * 60 * 1000)
+    public void getAverageSearchFrequencyForOneHour() {
+        log.info("--Start Scheduled--");
+        getAverageSearchFrequencyForOneHours();
+        log.info("--End Scheduled--");
+    }
+
+    private Set<ZSetOperations.TypedTuple<String>> getAllSearchKeywordForOneHour(String keyValue) {
+        return redisTemplate.opsForZSet().rangeByScoreWithScores(keyValue, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+    }
+
+    private Double findSearchKeywordWeight(String keyword) {
 
         String weightKey = SEARCH_WEIGHT_PREFIX + keyword;
         String weightString = redisTemplate.opsForValue().get(weightKey);
 
-        if (weightString != null) {
-            double weight = Double.parseDouble(weightString);
-            redisTemplate.opsForZSet().incrementScore("search_ranking", keyword, weight);
-        } else {
-            redisTemplate.opsForZSet().incrementScore("search_ranking", keyword, 1);
+        if(weightString != null) {
+            return Double.parseDouble(weightString);
         }
+
+        return 1.0;
     }
 
-    // 1시간 마다 평균 검색 횟수를 구하는 메서드
-    @Scheduled(fixedRate = 60 * 60 * 1000)
-    private void getAverageSearchFrequencyForOneHour() {
-
-        log.info("--Scheduled--");
-
-        long currentTime = Instant.now().getEpochSecond();
-        long oneHourAgo = currentTime - (60 * 60);
-
-        Set<ZSetOperations.TypedTuple<String>> searchKeywords = getAllSearchKeywordForOneHour(currentTime, oneHourAgo);
-        double totalSearchFrequency = 0;
-
-        for (ZSetOperations.TypedTuple<String> searchKeyword : searchKeywords) {
-            if (searchKeyword != null) {
-                Double searchFrequency = searchKeyword.getScore();
-                if(searchFrequency != null) {
-                    totalSearchFrequency += searchFrequency;
-                }
-            }
-        }
+    //후에 Lock 을 적용시켜서 동시성 해결해야함 totalSearchFrequency++ 부분과 redisTemplate 부분 동시성 문제가 발생함 Redis 의 pub-sub Message 락을 걸 예정
+    @Async
+    public void getAverageSearchFrequencyForOneHours() {
+        String searchFrequencyWord = "search_keyword_forOneHour";
+        Set<ZSetOperations.TypedTuple<String>> searchKeywords = getAllSearchKeywordForOneHour(searchFrequencyWord);
 
         int numberOfSearchKeywords = searchKeywords.size();
 
-        if (numberOfSearchKeywords > 0) {
-            // 1시간 동안 검색된 평균 빈도수를 구함
-            double averageSearchFrequency = totalSearchFrequency / numberOfSearchKeywords;
+        log.info("searchKeywords_size : {}", numberOfSearchKeywords);
+
+        if(numberOfSearchKeywords > 0) {
             String keyword;
             Double searchFrequency;
+            double averageSearchFrequency = totalSearchFrequency / numberOfSearchKeywords;
 
-            // 검색어의 가중치를 계산하고 적용함
-            for (ZSetOperations.TypedTuple<String> searchKeyword : searchKeywords) {
-                if (searchKeyword != null) {
+            log.info("averageSearchFrequency : {}", averageSearchFrequency);
+
+            if(averageSearchFrequency > 0) {
+                for (ZSetOperations.TypedTuple<String> searchKeyword : searchKeywords) {
                     keyword = searchKeyword.getValue();
                     searchFrequency = searchKeyword.getScore();
+                    double weight;
 
-                    if (searchFrequency != null) {
-                        // 검색 빈도에 따라 가중치를 계산하여 적용
-                        double weight = searchFrequency / averageSearchFrequency;
+                    if (searchFrequency != null && searchFrequency != 0) {
+                        if(searchFrequency > averageSearchFrequency) {
+                            //검색 빈도에 따라 가중치를 계산
+                            weight = searchFrequency / averageSearchFrequency;
+                        } else {
+                            weight = 1.0;
+                        }
 
                         // 가중치를 새로운 키에 저장 (search_weight:{검색어})
                         String weightKey = SEARCH_WEIGHT_PREFIX + keyword;
@@ -76,10 +92,9 @@ public class RedisService {
                 }
             }
         }
-    }
 
-    private Set<ZSetOperations.TypedTuple<String>> getAllSearchKeywordForOneHour(long currentTime, long oneHourAgo) {
-        return redisTemplate.opsForZSet().rangeByScoreWithScores("search_ranking", oneHourAgo, currentTime);
+        redisTemplate.opsForZSet().removeRange("search_keyword_forOneHour", 0, -1);
+        totalSearchFrequency = 0;
     }
 
 }
